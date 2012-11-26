b1f86eedd0a8958314b1c58878c64b84b2244878
Christopher D. Long <galizur@gmail.com>
2012-11-25 21:46:49 -0800

    Update for 11/24 games

--- bbref_sos.sh
+++ bbref_sos.sh
@@ -14,3 +14,5 @@ psql basketball -f bbref_sos/schedule_factors.sql
 
 psql basketball -f bbref_sos/current_ranking.sql > bbref_sos/current_ranking.txt
 psql basketball -f bbref_sos/predict_playoffs.sql > bbref_sos/predict_playoffs.txt
+
+psql basketball -f bbref_sos/predict_daily.sql > bbref_sos/predict_daily.txt
---
+++ bbref_sos/predict_daily.sql
@@ -0,0 +1,21 @@
+select
+--p.game_date as date,
+p.home_id as home,
+p.visitor_id as opp,
+(h.strength*o.exp_factor)::numeric(4,2) as h_str,
+(v.strength*d.exp_factor)::numeric(4,2) as o_str,
+(
+(h.strength*o.exp_factor)^16/
+((h.strength*o.exp_factor)^16+(v.strength*d.exp_factor)^16)
+)::numeric(4,2) as pr_home
+from bbref.games p
+join bbref._schedule_factors v
+  on (v.year,v.team_id)=(p.year,p.visitor_id)
+join bbref._schedule_factors h
+  on (h.year,h.team_id)=(p.year,p.home_id)
+join bbref._factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join bbref._factors d
+  on (d.parameter,d.level)=('field','defense_home')
+where p.game_date::date=current_date-1
+order by p.home_id asc;
