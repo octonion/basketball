908aaa6c4e174ff14d2520d22a3ca3ce00771713
Christopher Long <galizur@gmail.com>
2015-03-17 05:40:28 -0400

    NCAA update for 2014-15.

--- ncaa_sr/sos/predict_daily.sql
+++ ncaa_sr/sos/predict_daily.sql
@@ -1,19 +1,20 @@
 begin;
 
-set timezone to 'America/Los_Angeles';
+set timezone to 'America/New_York';
 
 select
 g.game_date::date as date,
+'home' as site,
 hn.school_name as home,
-vn.school_name as opp,
-(exp(i.estimate)*y.exp_factor*h.offensive*o.exp_factor*v.defensive)::numeric(4,1) as t_score,
-(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive*d.exp_factor)::numeric(4,1) as o_score,
-(h.strength*o.exp_factor)::numeric(4,2) as h_str,
-(v.strength*d.exp_factor)::numeric(4,2) as o_str,
+(h.strength*o.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*o.exp_factor*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength*d.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive*d.exp_factor)::numeric(4,1) as score,
 (
-(h.strength*o.exp_factor)^10/
-((h.strength*o.exp_factor)^10+(v.strength*d.exp_factor)^10)
-)::numeric(4,2) as pr_home
+(h.strength*o.exp_factor)^10.25/
+((h.strength*o.exp_factor)^10.25+(v.strength*d.exp_factor)^10.25)
+)::numeric(4,2) as pwin
 from ncaa_sr.games g
 join ncaa_sr.schools hn
   on (hn.school_id)=(g.school_id)
@@ -34,6 +35,110 @@ join ncaa_sr._basic_factors i
 where
     g.game_date::date = current_date
 and g.location is null
-order by g.game_date::date,g.school_id asc;
+
+union all
+
+select
+g.game_date::date as date,
+'neutral' as site,
+hn.school_name as home,
+(h.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive)::numeric(4,1) as score,
+(
+(h.strength)^10.25/
+((h.strength)^10.25+(v.strength)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date = current_date
+and g.location='N'
+
+order by home asc;
+
+copy
+(
+select
+g.game_date::date as date,
+'home' as site,
+hn.school_name as home,
+(h.strength*o.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*o.exp_factor*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength*d.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive*d.exp_factor)::numeric(4,1) as score,
+(
+(h.strength*o.exp_factor)^10.25/
+((h.strength*o.exp_factor)^10.25+(v.strength*d.exp_factor)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join ncaa_sr._factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date = current_date
+and g.location is null
+
+union all
+
+select
+g.game_date::date as date,
+'neutral' as site,
+hn.school_name as home,
+(h.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive)::numeric(4,1) as score,
+(
+(h.strength)^10.25/
+((h.strength)^10.25+(v.strength)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date = current_date
+and g.location='N'
+
+order by home asc
+) to '/tmp/predict_daily.csv';
 
 commit;
--- ncaa_sr/sos/predict_weekly.sql
+++ ncaa_sr/sos/predict_weekly.sql
@@ -1,21 +1,144 @@
+begin;
+
+set timezone to 'America/New_York';
+
 select
-p.home_id as home,
-p.game_date as date,
-p.visitor_id as opp,
-(h.strength*o.exp_factor)::numeric(4,2) as h_str,
-(v.strength*d.exp_factor)::numeric(4,2) as o_str,
+g.game_date::date as date,
+'home' as site,
+hn.school_name as home,
+(h.strength*o.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*o.exp_factor*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength*d.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive*d.exp_factor)::numeric(4,1) as score,
 (
-(h.strength*o.exp_factor)^16/
-((h.strength*o.exp_factor)^16+(v.strength*d.exp_factor)^16)
-)::numeric(4,2) as pr_home
-from ncaa_sr.games p
+(h.strength*o.exp_factor)^10.25/
+((h.strength*o.exp_factor)^10.25+(v.strength*d.exp_factor)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
 join ncaa_sr._schedule_factors v
-  on (v.year,v.team_id)=(p.year,p.visitor_id)
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
 join ncaa_sr._schedule_factors h
-  on (h.year,h.team_id)=(p.year,p.home_id)
+  on (h.year,h.team_id)=(g.year,g.school_id)
 join ncaa_sr._factors o
   on (o.parameter,o.level)=('field','offense_home')
 join ncaa_sr._factors d
   on (d.parameter,d.level)=('field','defense_home')
-where p.game_date::date between current_date-3 and current_date+6
-order by p.game_date::date,p.home_id asc;
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date between current_date and current_date+6
+and g.location is null
+
+union all
+
+select
+g.game_date::date as date,
+'neutral' as site,
+hn.school_name as home,
+(h.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive)::numeric(4,1) as score,
+(
+(h.strength)^10.25/
+((h.strength)^10.25+(v.strength)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date between current_date and current_date+6
+and g.location='N'
+
+order by home asc;
+
+copy
+(
+select
+g.game_date::date as date,
+'home' as site,
+hn.school_name as home,
+(h.strength*o.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*o.exp_factor*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength*d.exp_factor)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive*d.exp_factor)::numeric(4,1) as score,
+(
+(h.strength*o.exp_factor)^10.25/
+((h.strength*o.exp_factor)^10.25+(v.strength*d.exp_factor)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join ncaa_sr._factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date between current_date and current_date+6
+and g.location is null
+
+union all
+
+select
+g.game_date::date as date,
+'neutral' as site,
+hn.school_name as home,
+(h.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*h.offensive*v.defensive)::numeric(4,1) as score,
+vn.school_name as away,
+(v.strength)::numeric(4,2) as str,
+(exp(i.estimate)*y.exp_factor*v.offensive*h.defensive)::numeric(4,1) as score,
+(
+(h.strength)^10.25/
+((h.strength)^10.25+(v.strength)^10.25)
+)::numeric(4,2) as pwin
+from ncaa_sr.games g
+join ncaa_sr.schools hn
+  on (hn.school_id)=(g.school_id)
+join ncaa_sr.schools vn
+  on (vn.school_id)=(g.opponent_id)
+join ncaa_sr._schedule_factors v
+  on (v.year,v.team_id)=(g.year,g.opponent_id)
+join ncaa_sr._schedule_factors h
+  on (h.year,h.team_id)=(g.year,g.school_id)
+join ncaa_sr._factors y
+  on (y.parameter,y.level)=('year',g.year::text)
+join ncaa_sr._basic_factors i
+  on (i.factor)=('(Intercept)')
+where
+    g.game_date::date between current_date and current_date+6
+and g.location='N'
+
+order by home asc
+) to '/tmp/predict_weekly.csv';
+
+commit;
