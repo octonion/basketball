4b486043d28b2a90bc88d30db915d5903ca54f5e
Christopher Long <galizur@gmail.com>
2015-03-18 17:34:34 -0400

    NCAA women's update for 2014-15.

--- ncaa_women/sos.sh
+++ ncaa_women/sos.sh
@@ -9,11 +9,21 @@ psql basketball -c "vacuum analyze ncaa_women.results;"
 
 R --vanilla -f sos/lmer.R
 
+psql basketball -c "vacuum full verbose analyze ncaa_women._basic_factors;"
+psql basketball -c "vacuum full verbose analyze ncaa_women._parameter_levels;"
+
 psql basketball -f sos/normalize_factors.sql
+
+psql basketball -c "vacuum full verbose analyze ncaa_women._factors;"
+
 psql basketball -f sos/schedule_factors.sql
 
+psql basketball -c "vacuum full verbose analyze ncaa_women._schedule_factors;"
+
 psql basketball -f sos/connectivity.sql > sos/connectivity.txt
+
 psql basketball -f sos/current_ranking.sql > sos/current_ranking.txt
+
 psql basketball -f sos/division_ranking.sql > sos/division_ranking.txt
 
 psql basketball -f sos/test_predictions.sql > sos/test_predictions.txt
