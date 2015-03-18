5dc091d47fc20a95c3673c0d8c58ac86e2afcb02
Christopher Long <galizur@gmail.com>
2015-03-18 17:42:58 -0400

    NCAA tournament regional strengths.

--- ncaa_sr/tournament/region_ranks.sql
+++ ncaa_sr/tournament/region_ranks.sql
@@ -1,11 +1,11 @@
 select
 region_id as region,
-(exp(avg(log(ncaa_order::float))))::numeric(3,2) as ncaa_ao,
-(exp(avg(log(octonion_order::float))))::numeric(3,2) as octonion_ao,
-(exp(avg(log(octonion_rank::float))))::numeric(3,2) as octonion_ar,
-avg(ncaa_order::float)::numeric(3,1) as ncaa_ho,
-avg(octonion_order::float)::numeric(3,1) as octo_ho,
-avg(octonion_rank::float)::numeric(3,1) as octo_hr
+avg(ncaa_order::float)::numeric(3,1) as ncaa_ao,
+avg(octonion_order::float)::numeric(3,1) as octo_ao,
+avg(octonion_rank::float)::numeric(3,1) as octo_ar,
+(exp(avg(log(ncaa_order::float))))::numeric(3,2) as ncaa_ho,
+(exp(avg(log(octonion_order::float))))::numeric(3,2) as octonion_ho,
+(exp(avg(log(octonion_rank::float))))::numeric(3,2) as octonion_hr
 from ncaa_sr.rankings
 where ncaa_order not in (44,46,66,68)
 group by region
@@ -13,12 +13,12 @@ order by region;
 
 select
 region_id as region,
-(exp(avg(log(ncaa_order::float))))::numeric(3,2) as ncaa_ao,
-(exp(avg(log(octonion_order::float))))::numeric(3,2) as octonion_ao,
-(exp(avg(log(octonion_rank::float))))::numeric(3,2) as octonion_ar,
-avg(ncaa_order::float)::numeric(3,1) as ncaa_ho,
-avg(octonion_order::float)::numeric(3,1) as octo_ho,
-avg(octonion_rank::float)::numeric(3,1) as octo_hr
+avg(ncaa_order::float)::numeric(3,1) as ncaa_ao,
+avg(octonion_order::float)::numeric(3,1) as octo_ao,
+avg(octonion_rank::float)::numeric(3,1) as octo_ar,
+(exp(avg(log(ncaa_order::float))))::numeric(3,2) as ncaa_ho,
+(exp(avg(log(octonion_order::float))))::numeric(3,2) as octonion_ho,
+(exp(avg(log(octonion_rank::float))))::numeric(3,2) as octonion_hr
 from ncaa_sr.rankings
 where
     ncaa_order > 5
