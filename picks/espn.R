55291808a41d1e23a4ab3565cf83c5b4411b3d92
Christopher Long <galizur@gmail.com>
2015-03-22 03:57:14 -0400

    NCAA power ranking from 432K ESPN NCAA tournament brackets.

---
+++ picks/espn.R
@@ -0,0 +1,29 @@
+library(BradleyTerry2)
+
+games <- read.csv("outcomes.csv", header=TRUE)
+teams <- read.csv("teams.csv", header=TRUE)
+
+games$team <- as.factor(games$team)
+games$opponent <- as.factor(games$opponent)
+
+head(games)
+
+dim(games)
+
+fit <- BTm(cbind(won, lost), team, opponent, data = games)
+
+espn <- as.data.frame(BTabilities(fit))
+espn$id <- rownames(espn)
+
+ranks <- merge(teams, espn)
+ranks <- ranks[with(ranks, order(-ability)), ]
+
+ranks <- subset(ranks, TRUE, select=c(name_long,ability))
+colnames(ranks)[1] <- "team"
+
+ranks$strength <- exp(ranks$ability)
+
+ranks <- cbind(rank=rank(-ranks$ability), ranks)
+
+ranks
+write.csv(ranks, file="ranks.csv", row.names=FALSE)
