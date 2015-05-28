cd543e05bd3de6c9e8d9d47eb255b7b4a4f3e7e4
Christopher D. Long <galizur@gmail.com>
2015-05-28 15:48:26 -0400

    Top NBA teams, 1977-2015.

---
+++ bbref/sos/nba_best.sql
@@ -0,0 +1,45 @@
+begin;
+
+create temporary table r (
+       rk	 serial,
+       team 	 text,
+       team_id	 text,
+       year	 integer,
+       str	 numeric(4,3),
+       ofs	 numeric(4,3),
+       dfs	 numeric(4,3),
+       sos	 numeric(4,3)
+);
+
+insert into r
+(team,team_id,year,str,ofs,dfs,sos)
+(
+select
+coalesce(s.team_name,sf.team_id::text),
+sf.team_id,
+sf.year,
+sf.strength::numeric(4,3) as str,
+offensive::numeric(4,3) as ofs,
+defensive::numeric(4,3) as dfs,
+schedule_strength::numeric(4,3) as sos
+from bbref._schedule_factors sf
+join bbref.teams s
+  on (s.team_id)=(sf.team_id)
+--where sf.year in (2015)
+order by str desc
+limit 100
+);
+
+select
+rk,year,team,str,ofs,dfs,sos
+from r
+order by rk asc;
+
+copy (
+select
+rk,year,team,str,ofs,dfs,sos
+from r
+order by rk asc
+) to '/tmp/nba_best.csv' csv header;
+
+commit;
