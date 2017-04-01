0b3f1548745e430507f917b8a71c6d282d67c43f
Christopher D. Long <galizur@gmail.com>
2017-04-01 14:41:07 -0400

    Coach valued added.

--- ncaa_sr/sos_reg/lmer.R
+++ ncaa_sr/sos_reg/lmer.R
@@ -28,7 +28,7 @@ ln(r.team_score::float) as log_ps
 from ncaa_sr.beta_results r
 where
 TRUE
-and r.year between 1980 and 2015
+and r.year between 1980 and 2017
 and r.type in ('REG','CTOURN')
 and r.team_score>0
 and r.opponent_score>0
